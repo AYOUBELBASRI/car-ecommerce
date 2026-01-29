e0e37c1f1fb4f63165b660b1890db7435c583265
MERYEM BAHADDOU <meryemba2003@gmail.com>
2026-01-29 10:50:31 +0100

    navbar color

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,6 +1,5 @@
-import { isPlatformBrowser, NgClass, AsyncPipe } from '@angular/common';
-import { Component, HostListener, inject, OnInit } from '@angular/core';
-import { PLATFORM_ID } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CartService } from '../cart/cart.service';
 import { Observable } from 'rxjs';
@@ -8,35 +7,17 @@ import { Observable } from 'rxjs';
 
 @Component({
     selector: 'app-navbar',
-    imports: [RouterLink, NgClass, AsyncPipe],
+    imports: [RouterLink, AsyncPipe],
     templateUrl: './navbar.html',
     styleUrl: './navbar.css',
 })
 export class Navbar implements OnInit {
-    isScrolled = false;
-    private readonly platformId = inject(PLATFORM_ID);
-    private readonly scrolledThreshold = 24;
     cartItemCount: Observable<number> | undefined;
 
-    constructor(private cartService: CartService) {
-        this.updateScrolledState();
-    }
+    constructor(private cartService: CartService) {}
 
     ngOnInit(): void {
         this.cartItemCount = this.cartService.getCartItemCount();
-    }
-
-    @HostListener('window:scroll')
-    onWindowScroll(): void {
-        this.updateScrolledState();
-    }
-
-    private updateScrolledState(): void {
-        if (!isPlatformBrowser(this.platformId)) {
-            return;
-        }
-
-        this.isScrolled = window.scrollY > this.scrolledThreshold;
     }
 }
 
