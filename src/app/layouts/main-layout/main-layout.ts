8b070db00cdaeea53938b68f6846bd61d651a3e9
AYOUBELBASRI <elbasri.ayoub15@gmail.com>
2026-02-03 15:31:49 +0100

    ayoub

--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { Navbar } from '../../navbar/navbar';
 import { Footer } from '../../footer/footer';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-main-layout',
@@ -10,4 +11,22 @@ import { Footer } from '../../footer/footer';
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css'
 })
-export class MainLayout {}
+export class MainLayout implements OnInit {
+  isHomeRoute = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.setRouteState(this.router.url);
+
+    this.router.events
+      .pipe(filter((event): event is NavigationEnd => event instanceof NavigationEnd))
+      .subscribe((event) => {
+        this.setRouteState(event.urlAfterRedirects);
+      });
+  }
+
+  private setRouteState(url: string): void {
+    this.isHomeRoute = url === '/home' || url.startsWith('/home?') || url.startsWith('/home#');
+  }
+}
